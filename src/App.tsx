d49160e78f01c57dc0169338161409389a50e0a7
JonashGana <jonashtiglao@gmail.com>
2025-03-26 23:03:54 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Introduction from "./sections/Introduction";
+import Features from "./sections/Features";
+import Integrations from "./sections/Integrations";
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-[#0A0A0A]">
+    <div className="min-h-screen w-full bg-[#0a0a0a]">
       <Navbar />
       <Hero />
+      <Introduction />
+      <Features />
+      <Integrations />
     </div>
   );
 }
